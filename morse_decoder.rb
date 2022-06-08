99ad2aef22ce0bc03d42de85b68e083d28be157f
Augusto <jesus_augga_99@icloud.com>
2022-06-08 12:33:10 -0500

    fix linter errors

--- morse_decoder.rb
+++ morse_decoder.rb
@@ -1,40 +1,23 @@
-def decode_char(decodedChar)
+def decode_char(decoded_char)
   character_translator = {
-    '.-' => 'A',
-    '-...' => 'B',
-    '-.-.' => 'C',
-    '-..' => 'D',
-    '.' => 'E',
-    '..-.' => 'F',
-    '--.' => 'G',
-    '....' => 'H',
-    '..' => 'I',
-    '.---' => 'J',
-    '-.-' => 'K',
-    '.-..' => 'L',
-    '--' => 'M',
-    '-.' => 'N',
-    '---' => 'O',
-    '.--.' => 'P',
-    '--.-' => 'Q',
-    '.-.' => 'R',
-    '...' => 'S',
-    '-' => 'T',
-    '..-' => 'U',
-    '...-' => 'V',
-    '.--' => 'W',
-    '-..-' => 'X',
-    '-.--' => 'Y',
-    '--..' => 'Z'
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
+    '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
+    '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+    '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
+    '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+    '-.--' => 'Y', '--..' => 'Z'
   }
- character_translator[decodedChar]
+  character_translator[decoded_char]
 end
 
-def decode_word(decodedWord)
-  decodedWord.split(' ').map { |char| decode_char(char) }.join
+def decode_word(decoded_word)
+  decoded_word.split.map { |char| decode_char(char) }.join
 end
 
-def decode(decodeSentence)
-  decodeSentence.split('   ').map { |word| decode_word(word) }.join(' ')
+def decode(decode_sentence)
+  decode_sentence.split('   ').map { |word| decode_word(word) }.join(' ')
 end
+
 print decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+# Prints: "A BOX FULL OF RUBIES"
